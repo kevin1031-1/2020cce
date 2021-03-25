fe768548c6562c4bc5437013d2bca070ac42834d
kevin1031-1 <79627929+kevin1031-1@users.noreply.github.com>
2021-03-25 16:47:46 +0800

    Add files via upload

---
+++ week05/week05(4).cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char a[5][10]={"decline","proper","majority","bullet","shop"};
+    char *p;
+    for(int i=0;i<5;i++)
+    {
+
+        printf("%s\n",a[i]);
+    }
+
+}
